a78852c2de78740a87ad60ad68a5ec11eb5294c4
WriteCodeChangeWorld <lanou3g@houhou.local>
2014-06-10 16:54:22 +0800

    1.1
    没写完

---
+++ UI_gitHup/UI_gitHup/RoorViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoorViewController.h
+//  UI_gitHup
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoorViewController : UIViewController
+
+@end
---
+++ UI_gitHup/UI_gitHup/RoorViewController.m
@@ -0,0 +1,38 @@
+//
+//  RoorViewController.m
+//  UI_gitHup
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import "RoorViewController.h"
+
+@interface RoorViewController ()
+
+@end
+
+@implementation RoorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
